6f3a5edf6909620a20025d15789fa2fc838cf1bf
joh4nrodriguez <jsrodrguez2@misena.edu.co>
2022-09-07 11:37:13 -0500

    a

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
 
+from accounts.models import Account
+
 # Create your views here.
 
 
 def registro(request):
-    return render(request, 'registro.html');
+    context ={}
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirmPassword = request.POST['confirmPassword']
+        nombreUsuario = request.POST['username']
+        email = request.POST['correo']
+    
+        #Validacion campos
+        ok = True
+        if not email:
+         context['alarma'] = 'Ingrese el correo electronico'
+         ok = False
+        if not password or len(password) < 5:
+         context['alarma'] = 'Ingrese un password de cinco (5) o mas caracteres'
+         ok = False  
+        if password != confirmPassword:
+         context['alarma'] = '¡El password no coincide!'
+         ok = False
+         
+        #Todo ok
+        if ok:
+            existe = Account.object.filter(email=email).exists()
+            if not existe:
+                user = Account.objects.create_user(first_name=nombreUsuario, last_name=nombreUsuario, username=nombreUsuario, email=email, password=password)
+                user.save()
+                context['alarma'] = 'Usuario guardado con exito!'
+            else:
+                context['alarma'] = '¡El correo ya existe!'
+                
+    return render(request, 'registro.html', context);
+
