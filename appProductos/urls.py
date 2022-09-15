f0fd54723e6776c06eaa751bd8fe5c208a0b70e4
adriany31 <amontaa9@misena.edu.co>
2022-09-15 08:06:17 -0500

    a

--- appProductos/urls.py
+++ appProductos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views 
 from django.urls import include
 urlpatterns = [
-  path('producto')
+  path('producto/', views.verProductos, name="productos"),
+  path('producto/<str:id>', views.verProductos, name="producto"),
 ]
