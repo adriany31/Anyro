21d2ee7bf762232af257c4b869cffcc60a550284
joh4nrodriguez <jsrodrguez2@misena.edu.co>
2022-09-15 10:04:26 -0500

    ac

--- anyro/urls.py
+++ anyro/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from django.conf.urls.static import static
 from anyro import settings
+
 from . import views 
 from django.urls import include
 urlpatterns = [
--- appProductos/urls.py
+++ appProductos/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views 
-from django.urls import include
+
+
 urlpatterns = [
-  path('producto')
-  path('producto/', views.verProductos, name="productos"),
-  path('producto/<str:id>', views.verProductos, name="producto"),
+    path('producto/', views.verProductos, name="productos"),
+    path('producto/<str:id>', views.verProductos, name="producto"),
 ]
