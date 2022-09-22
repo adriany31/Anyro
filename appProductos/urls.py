6bcb9fcc5417db9b58be1b8d2c79bb1debc869cb
joh4nrodriguez <jsrodrguez2@misena.edu.co>
2022-09-22 10:18:25 -0500

    a

--- appProductos/urls.py
+++ appProductos/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('producto/<str:id>', views.verProductos, name="producto"),
     path('carrito/<str:id>', views.agregar, name="carrito"),
     path('vercarrito/', views.verCarrito, name="ver_carrito"),
+    path('eliminar/', views.eliminarItemCarrito, name="eliminar"),
+    
 ]
